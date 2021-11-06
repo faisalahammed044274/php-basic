741217bf9e2790eac7bddc8b89f2ab3da5e58dd9
faisalahammed044274 <setu2014@gmail.com>
2021-11-07 02:35:51 +0600

    loops

--- 07_loops.php
+++ 07_loops.php
@@ -33,10 +33,34 @@ $counter = 0;
 // for
 //==================================
 
+for ($i = 0; $i < 10; $i++) {
+    echo $i. '<br>';
+}
+
 //==================================
 // foreach
 //==================================
 
+$fruits = ["Banana", "Apple", "Orange"];
+foreach ($fruits as $i => $fruit) {
+    echo $i .' '. $fruit . "<br>";
+}
+
 //==================================
 // Iterate Over associative array.
 //==================================
+echo "<hr>";
+$person = [
+    'name' => 'Faisal',
+    'surname' => 'Setu',
+    'age' => 30,
+    'hobbies' => ['Tennis','Video games'],
+];
+
+foreach ($person as $key => $value){
+    if (is_array($value)){
+    echo $key .' '. implode(",", $value) . "<br>";
+} else {
+    echo $key . ' ' . $value . '<br>';
+}
+}
