7a4ad481f92f2d9c0032ac6ece6123d9e1581e24
faisalahammed044274 <setu2014@gmail.com>
2021-11-08 02:14:18 +0600

    curl

--- 13_curl/index.php
+++ 13_curl/index.php
@@ -2,7 +2,15 @@
 
 $url = 'https://jsonplaceholder.typicode.com/users';
 // Sample example to get data.
+$resource = curl_init($url);
+curl_setopt($resource , CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($resource);
+$info = curl_getinfo($resource);
+echo '<pre>';
+var_dump($info);
+echo '</pre>';
 
+// echo $result;
 // Get response status code
 
 // set_opt_array
