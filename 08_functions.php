256dc1abe4d7d0a9208175c6db54e78a27cdc625
faisalahammed044274 <setu2014@gmail.com>
2021-11-07 16:07:59 +0600

    php crush course

--- 08_functions.php
+++ 08_functions.php
@@ -1,11 +1,47 @@
 <?php
 
-// Function which prints "Hello I am Zura"
-
+// Function which prints "Hello I am Setu"
+// function hello(){
+//     echo "Hello I am Faisal" . '<br>';
+// }
+// hello();
 // Function with argument
+
+// function helloOne($name){
+//     echo "Hello I am $name" . "<br>";
+// }
+// helloOne('Setu') . "<br>";
+// helloOne('Faisal Ahammmed') . "<br>";
+
+// function helloOne($name){
+//     return "Hello I am $name" . "<br>";
+// }
+// echo helloOne('Setu');
+// echo helloOne('Faisal Ahammmed');
 
 // Create sum of two functions
 
+// function sum($a, $b){
+//     return $a + $b;
+// }
+// echo sum(4,6);
 // Create function to sum all numbers using ...$nums
 
+// function sum(...$nums)
+// {
+//     $sum = 0;
+//     foreach ($nums as $n){
+//     $sum += $n;
+//     }
+//     return $sum;
+// }
+
+// echo sum(1,2,3,4,5,6); 
+
 // Arrow functions
+
+// function sum(...$nums)
+// {
+//     return array_reduce($nums , fn($carry, $n) => $carry + $n);
+// }
+// sum (1,2,3,4,5);
