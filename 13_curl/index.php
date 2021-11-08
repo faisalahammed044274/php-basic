54be4d3039b72997094ac58a44d2c8db8ff69336
faisalahammed044274 <setu2014@gmail.com>
2021-11-08 23:39:45 +0600

    php

--- 13_curl/index.php
+++ 13_curl/index.php
@@ -4,13 +4,16 @@ $url = 'https://jsonplaceholder.typicode.com/users';
 
 // Sample example to get data.
 
-$resource = curl_init($url);
-curl_setopt($resource, CURLOPT_RETURNTRANSFER, true);
-$result = curl_exec($resource);
-$info = curl_getinfo($resource, CURLINFO_HTTP_CODE);
-var_dump($info);
-echo $result;
+// $resource = curl_init($url);
 
+// curl_setopt($resource, CURLOPT_RETURNTRANSFER, true);
+// $result = curl_exec($resource);
+
+// $code = curl_getinfo($resource, CURLINFO_HTTP_CODE);
+
+// var_dump($code);
+
+// echo $result;
 
 // echo $result;
 // Get response status code
@@ -19,6 +22,24 @@ echo $result;
 
 // Post request
 
+$resource =curl_init();
+$user = [
+    'name' => 'Faisal Ahammed',
+    'username' => 'setufaisal',
+    'email' => 'setu@example.com'
+];
+curl_setopt($resource, $user[
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_POST => true,
+    CURLOPT_HTTPHEADER =>['content-type : application'],
+    CURLOPT_POSTFIELDS => json_encode($user)
+]);
+
+$result = curl_exec($resource);
+
+curl_close($resource);
+echo $result;
 
 
 ?>
